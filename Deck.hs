a2cdba5eb3afc930ec3e115e259e34bb7a47e65f
Victor Hugo Oliveira Leão <victor.pessoal1203@gmail.com>
2023-05-01 12:11:57 -0300

    deck embaralhado

--- Deck.hs
+++ Deck.hs
@@ -1,11 +1,24 @@
 import Types
 
+import System.Random
+import System.Random.Shuffle
+
+-- Cria as cartas de Number com todas as cores possíveis 4x
 listNumberColor :: [Card]
-listNumberColor = concatMap (\color -> map (\value -> Card Number color value) [One, Two, Three, Four, Five, Six, Seven, Eight, Nine]) [Red, Blue, Green, Yellow]
+listNumberColor = concatMap (\color -> concatMap (\value -> replicate 4 $ Card Number color value) [One, Two, Three, Four, Five, Six, Seven, Eight, Nine]) [Red, Blue, Green, Yellow]
+
+-- Cria as cartas de Reverse e Block com todas as cores possíveis 4x
+listSpecialsColor :: [Card]
+listSpecialsColor = concatMap (\color -> concatMap (\typecard -> replicate 4 $ Card typecard color NoValue) [Reverse, Block]) [Red, Blue, Green, Yellow]
+
+deck :: [Card]
+deck = listNumberColor ++ listSpecialsColor
 
 cardToString :: Card -> String
 cardToString (Card typeCard color value) = show typeCard ++ " " ++ show color ++ " " ++ show value
 
 main :: IO ()
 main = do
-  mapM_ (putStrLn . cardToString) listNumberColor
+  --mapM_ (putStrLn . cardToString) deck -- Deck normal
+  shuffled <- shuffleM deck
+  mapM_ (putStrLn . cardToString) shuffled -- Deck embaralhado
