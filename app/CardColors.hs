3f76620c97655ee25bd99ff101a1eff329d7a77d
Lameque Fernandes <lamequesao@gmail.com>
2023-05-04 15:16:36 -0300

    funcoes para mostrar cartas de acordo com cor e numero

---
+++ app/CardColors.hs
@@ -0,0 +1,85 @@
+module CardColors
+(numerosVermelhos
+,numerosAmarelos
+,numerosAzuis
+,numerosVerdes
+,cardValueNumber
+,arrayColorCard
+) where 
+
+import Types
+
+
+numerosVermelhos = [ "\x1b[31m0\x1b[0m"
+                   , "\x1b[31m1\x1b[0m"
+                   , "\x1b[31m2\x1b[0m"
+                   , "\x1b[31m3\x1b[0m"
+                   , "\x1b[31m4\x1b[0m"
+                   , "\x1b[31m5\x1b[0m"
+                   , "\x1b[31m6\x1b[0m"
+                   , "\x1b[31m7\x1b[0m"
+                   , "\x1b[31m8\x1b[0m"
+                   , "\x1b[31m9\x1b[0m"
+                   , "\x1b[31m+2\x1b[0m"
+                   ]
+
+numerosAmarelos = [ "\x1b[33m0\x1b[0m"
+                 , "\x1b[33m1\x1b[0m"
+                 , "\x1b[33m2\x1b[0m"
+                 , "\x1b[33m3\x1b[0m"
+                 , "\x1b[33m4\x1b[0m"
+                 , "\x1b[33m5\x1b[0m"
+                 , "\x1b[33m6\x1b[0m"
+                 , "\x1b[33m7\x1b[0m"
+                 , "\x1b[33m8\x1b[0m"
+                 , "\x1b[33m9\x1b[0m"
+                 , "\x1b[33m+2\x1b[0m"
+                 ]
+
+numerosAzuis = [ "\x1b[34m0\x1b[0m"
+              , "\x1b[34m1\x1b[0m"
+              , "\x1b[34m2\x1b[0m"
+              , "\x1b[34m3\x1b[0m"
+              , "\x1b[34m4\x1b[0m"
+              , "\x1b[34m5\x1b[0m"
+              , "\x1b[34m6\x1b[0m"
+              , "\x1b[34m7\x1b[0m"
+              , "\x1b[34m8\x1b[0m"
+              , "\x1b[34m9\x1b[0m"
+              , "\x1b[34m+2\x1b[0m"
+              ]
+
+numerosVerdes = [ "\x1b[32m0\x1b[0m"
+               , "\x1b[32m1\x1b[0m"
+               , "\x1b[32m2\x1b[0m"
+               , "\x1b[32m3\x1b[0m"
+               , "\x1b[32m4\x1b[0m"
+               , "\x1b[32m5\x1b[0m"
+               , "\x1b[32m6\x1b[0m"
+               , "\x1b[32m7\x1b[0m"
+               , "\x1b[32m8\x1b[0m"
+               , "\x1b[32m9\x1b[0m"
+               , "\x1b[32m+2\x1b[0m"
+               ]
+
+
+-- Retorna o valor numérico do cartão
+cardValueNumber :: Value -> Int
+cardValueNumber (Zero)    = 0
+cardValueNumber (One)     = 1
+cardValueNumber (Two)     = 2
+cardValueNumber (Three)   = 3
+cardValueNumber (Four)    = 4
+cardValueNumber (Five)    = 5
+cardValueNumber (Six)     = 6
+cardValueNumber (Seven)   = 7
+cardValueNumber (Eight)   = 8
+cardValueNumber (Nine)    = 9
+cardValueNumber _ = -1
+
+arrayColorCard :: Color -> [String]
+arrayColorCard (Red) = numerosVermelhos
+arrayColorCard (Blue) = numerosAzuis
+arrayColorCard (Green) = numerosVerdes
+arrayColorCard (Yellow) = numerosAmarelos
+arrayColorCard _ = [""]
--- app/Msg.hs
+++ app/Msg.hs
@@ -1,8 +1,14 @@
-module Msg 
-(msgWin
-,msgWelcome
-,msgNumPlayers
-) where
+-- module Msg 
+-- (msgWelcome
+-- ,msgNumPlayers
+-- ,displayCard
+-- ) where
+
+module Main where
+
+import CardColors
+import Types
+
 
 msgWelcome :: IO()
 msgWelcome = do
@@ -59,3 +65,31 @@ msgNumPlayers = do
     putStrLn "|                                        |4| QUADRO JOGADORES                                     |" 
     putStrLn "|                                        |0| SAIR DO JOGO                                         |" 
     putStrLn "|_________________________________________________________________________________________________|"
+
+-- msgNamePlayers :: String -> IO()
+-- msgNamePlayers numero = do
+--     putStrLn ("numero = " ++ numero)
+
+displayCard :: Card -> IO()
+displayCard (Card _ color value) = do
+    let index = cardValueNumber value
+    let arrayColor = arrayColorCard color
+
+    putStrLn ".—————."
+    putStrLn "|     |"
+    putStrLn ("|  " ++ (arrayColor !! index) ++ "  |")
+    putStrLn "|     |"
+    putStrLn "·—————·"
+
+
+main :: IO ()
+main = do
+    let card = Card Number Red Zero
+    let card2 = Card Number Yellow Seven
+    let card3 = Card Number Green Nine
+    let card4 = Card Number Blue One
+    displayCard card
+    displayCard card2
+    displayCard card3
+    displayCard card4
+
