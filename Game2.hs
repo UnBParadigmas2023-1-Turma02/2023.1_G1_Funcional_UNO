606ab7e3816fd760b047cd1cdb66e6b71f283238
leonardomilv3 <leonardomilov@gmail.com>
2023-05-04 09:03:08 -0300

    :memo: adicionando método readPlayers

--- Game2.hs
+++ Game2.hs
@@ -9,6 +9,19 @@ import System.IO
 import System.Random
 import System.Random.Shuffle
 
+
+readPlayers :: IO [Player]
+readPlayers = do
+  putStr "Enter number of players: "
+  numPlayersStr <- getLine
+  let numPlayers = read numPlayersStr :: Int
+  players <- mapM (\n -> do
+                    let i = 1
+                    putStr ("Enter name for player: ")
+                    playerName <- getLine
+                    return (playerName, [])) [1..numPlayers]
+  return players
+
 main :: IO ()
 main = do
   -- Imprimir Deck
@@ -17,11 +30,12 @@ main = do
   -- Deck embaralhado -> https://hackage.haskell.org/package/random-shuffle-0.0.4/docs/System-Random-Shuffle.html
   deckShuffled <- shuffleM deck
 
+  -- Lê os players do jogo
+  newPlayers <- readPlayers
+
   -- Distribuindo 7 cartas para N jogadores
-  --let (players, remainingDeck) = dealCards deckShuffled [("Player1", []), ("Player2", [])]
-  let (players, remainingDeck) = dealCards deckShuffled [("Player1", []), ("Player2", []), ("Player3", [])]
-  -- let (players, remainingDeck) = dealCards deckShuffled [("Player1", []), ("Player2", []), ("Player3", []), ("Player4", [])]
-  
+  let (players, remainingDeck) = dealCards deckShuffled newPlayers
+
   -- Imprime mão embaralhada dos jogadores
   -- mapM_ (\(name, hand) -> putStrLn (name ++ "'s hand: " ++ show hand)) players
 
