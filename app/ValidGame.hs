531027d9ea57c3e35a417fe8c69eded65f4c7918
Victor Hugo Oliveira Leão <victor.pessoal1203@gmail.com>
2023-05-05 08:29:47 -0300

    fix na validgame para buy four

--- app/ValidGame.hs
+++ app/ValidGame.hs
@@ -8,4 +8,5 @@ validCard (Card typeCard1 color1 value1) (Card typeCard2 color2 value2) --
  |color1 == color2 = True --mesma cor
  |value1 == value2 = True --mesmo valor
  |(typeCard1 /= Number &&  typeCard2 /= Number) && typeCard1 == typeCard2 = True --mesmo tipo
+ |(color2 == NoColor &&  typeCard2 == Buy) || (typeCard2 == Only_Color) = True -- buy four
  |otherwise = False
