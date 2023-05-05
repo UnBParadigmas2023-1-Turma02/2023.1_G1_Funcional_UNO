63df2f95f7a9a15b5643633cdf2669802d6d1ff1
Lameque Fernandes <lamequesao@gmail.com>
2023-05-05 12:29:44 -0300

    correcao em espaçamento das cartas

--- app/UI/Utils.hs
+++ app/UI/Utils.hs
@@ -6,12 +6,12 @@ import Types
 
 contentCard :: Int -> [String] -> String
 contentCard i array
-    | i == 10 = "|  "++(array !! 10)++"  |"
-    | i == 11 = "|  "++(array !! 11)++"  |" 
-    | i == 12 = "|  "++(array !! 12)++"  |"
-    | i == 13 = "|  "++(array !! 13)++"  |"
-    | i == 14 = "| "++(array !! 14)++" |"
-    | otherwise = "|  "++(array !! i)++"   |"
+    | i == 10 = "| "++(array !! 10)++"  |"
+    | i == 11 = "| "++(array !! 11)++"  |" 
+    | i == 12 = "| "++(array !! 12)++" |"
+    | i == 13 = "|  "++(array !! 13)++" |"
+    | i == 14 = "|"++(array !! 14)++" |"
+    | otherwise = "|  "++(array !! i)++"  |"
 
 cardToString :: Card -> String
 cardToString (Card cardType color value) =
